e8d343895db8dc80f31d425c3210647d2ecdf6af
rodrigo <rodrigo_c91@hotmail.com>
2024-07-12 09:40:23 -0300

    iniciado projeto backend

---
+++ smarthint-backend/src/main/java/com/smarthint/clientes/smarthint_backend/SmarthintBackendApplication.java
@@ -0,0 +1,13 @@
+package com.smarthint.clientes.smarthint_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmarthintBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmarthintBackendApplication.class, args);
+	}
+
+}
---
+++ smarthint-backend/src/test/java/com/smarthint/clientes/smarthint_backend/SmarthintBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smarthint.clientes.smarthint_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmarthintBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
